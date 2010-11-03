9e9542563b61f3a9d9bd2a629bff26b87817f371
Ross J. Reedstrom <reedstrm@cnx.org>
2010-11-03 20:58:46 +0000

    [releaser] preparing release 1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.4'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = read("Products", "CatalogMemberDataTool", "version.txt").strip()
 
 setup(name='Products.CatalogMemberDataTool',
       version=version,
